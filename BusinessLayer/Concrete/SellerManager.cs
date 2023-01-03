af11a2505b664ef93912ec3587e4cf4154549102
Emirhanerbil <emirhan117net@gmail.com>
2023-01-03 17:04:53 +0300

    Huhe

---
+++ BusinessLayer/Concrete/SellerManager.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SellerManager : ISellerService
+    {
+        ISellerDal sellerDal;
+
+
+
+        public SellerManager(ISellerDal sellerDal)
+        {
+            this.sellerDal=sellerDal;
+        }
+
+        public List<Seller> GetSeller()
+        {
+            return sellerDal.GetAll();
+        }
+
+        public Seller GetSellerByID(int id)
+        {
+            return sellerDal.GetById(id);
+        }
+
+        public List<Seller> GetSellerByName(string seller)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SellerAdd(Seller seller)
+        {
+            sellerDal.Insert(seller);
+        }
+
+        public void SellerRemove(Seller seller)
+        {
+            sellerDal.Delete(seller);
+        }
+
+        public void SellerUpdate(Seller seller)
+        {
+
+            sellerDal.Update(seller);
+        }
+    }
+}
---
+++ Review_Me/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Review_Me.Models;
+
+namespace Review_Me.Controllers
+{
+    public class ProfileController : Controller
+    {
+        SellerManager sm = new SellerManager(new EFSellerRepository());
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            ContentSellerUser su = new ContentSellerUser();
+            su.Sellers = context.Sellers.ToList();
+            su.Users = context.Users.ToList();
+            su.Contents = context.Contents.ToList();
+            su.Jobs = context.Jobs.ToList();
+
+            return View(su);
+        }
+
+        public IActionResult ProfilPage(int id)
+        {
+            var value = sm.GetSellerByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult ProfilPage(Seller seller)
+        {
+            ContentSellerUser su = new ContentSellerUser();
+            sm.SellerUpdate(seller);
+            su.seller2 = context.Sellers.ToList()[0];
+
+
+            return View(su);
+        }
+    }
+}
